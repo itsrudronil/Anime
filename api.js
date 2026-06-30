a1afd471ddd7c3c17fa6b04919d77230471cab36
itsrudronil <workforrudronil@gmail.com>
2026-06-30 14:03:06 +0600

    Implement getChannels function for live streams
    Added a function to fetch live streams using a proxy for CORS bypass.

--- api.js
+++ api.js
@@ -1,1 +1,13 @@
 
+// api.js
+const API_BASE = 'http://starter.nvisionbd.net/player_api.php?username=talukderrudronil&password=talRudronil8';
+
+export async function getChannels() {
+    const url = `${API_BASE}&action=get_live_streams`;
+    // CORS বাইপাস করার জন্য প্রক্সি ব্যবহার
+    const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
+    
+    const response = await fetch(proxyUrl);
+    const data = await response.json();
+    return JSON.parse(data.contents);
+}
