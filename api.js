ca00771082b80fcc05557ec5c3ca8ce7b081a61b
itsrudronil <workforrudronil@gmail.com>
2026-06-30 14:20:43 +0600

    Update getChannels to fetch from local channels.json
    Replaced API call with local JSON file retrieval.

--- api.js
+++ api.js
@@ -1,13 +1,4 @@
-
-// api.js
-const API_BASE = 'http://starter.nvisionbd.net/player_api.php?username=talukderrudronil&password=talRudronil8';
-
 export async function getChannels() {
-    const url = `${API_BASE}&action=get_live_streams`;
-    // CORS বাইপাস করার জন্য প্রক্সি ব্যবহার
-    const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
-    
-    const response = await fetch(proxyUrl);
-    const data = await response.json();
-    return JSON.parse(data.contents);
+    const response = await fetch('channels.json'); // তোমার লোকাল ফাইল
+    return await response.json();
 }
