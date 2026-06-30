95594a026af964155bb55aa2f6b33c1777001384
itsrudronil <workforrudronil@gmail.com>
2026-06-30 21:28:40 +0600

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-export async function getChannels() {
-    try {
-        const response = await fetch('channels.json?t=' + new Date().getTime());
-        return await response.json();
-    } catch (error) {
-        console.error("JSON লোড করতে সমস্যা:", error);
-        return [];
-    }
-}
